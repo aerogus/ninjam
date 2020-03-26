bc79f9017d8fa473d763e598fe24fea469efba08
Guillaume Seznec <767901+aerogus@users.noreply.github.com>
2020-03-26 18:53:16 +0100

    Update install.sh
    more verbose install

--- install.sh
+++ install.sh
@@ -7,25 +7,25 @@
 NINJAM_FILE="ninjam_server_0.06.tar.gz"
 NINJAM_URL="http://www.cockos.com/ninjam/downloads/src/$NINJAM_FILE"
 
-# prerequis
+echo "Installation dépendances"
 apt-get install build-essential wget ufw
 
-# téléchargement, décompression, compilation des sources
+echo "téléchargement, décompression, compilation des sources"
 wget "$NINJAM_URL"
 tar fvxz "$NINJAM_FILE" -C /opt
 rm "$NINJAM_FILE"
 cd /opt/ninjam_server_0.06/ninjam/server
 make
 
-# paramétrage
+echo "copie du paramétrage"
+mkdir /etc/ninjam /var/log/ninjam
 cp ./ninjam.cfg /etc/ninjam/server.cfg
 cp ./cclicence.txt /etc/ninjam/cclicense.txt
 cp ./ninjam.service /etc/systemd/system/ninjam.service
-mkdir /var/log/ninjam
 
-# ouverture du port du firewall
+echo "ouverture du port du firewall"
 ufw allow in 2049/tcp
 
-# activation au boot + direct
+echo "activation au boot + direct"
 systemctl enable ninjam
 systemctl start ninjam
